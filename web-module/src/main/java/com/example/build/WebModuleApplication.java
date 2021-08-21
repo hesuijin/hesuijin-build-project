77811d94e278a2c5936aa33a3588ca457de434eb
HSJ <hesuijin@qq.com>
2021-08-22 01:57:51 +0800

    hesuijin-build-project 初次提交

---
+++ web-module/src/main/java/com/example/build/WebModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebModuleApplication.class, args);
+    }
+
+}
