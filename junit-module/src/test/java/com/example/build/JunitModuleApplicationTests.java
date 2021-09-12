fb99e06ce94b8ac000cfa1862b6fd2dd4c3e14fc
HSJ <hesuijin@qq.com>
2021-09-12 17:36:49 +0800

    提交杂乱代码

---
+++ junit-module/src/test/java/com/example/build/JunitModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.build;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
